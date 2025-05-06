b87c74a4de08aea1d5ccff6dbab4f6a1c96e899e
bappy3a <16132152+bappy3a@users.noreply.github.com>
2025-05-06 09:34:08 +0000

    Fix styling

--- src/WeightConversionClass.php
+++ src/WeightConversionClass.php
@@ -2,21 +2,17 @@
 
 namespace Bappy3a\WeightConversion;
 
-class WeightConversionClass {
-
-    public static function kilograms(float $kilograms) : self
+class WeightConversionClass
+{
+    public static function kilograms(float $kilograms): self
     {
         return new static($kilograms);
     }
 
-    public function __construct(protected float $kilograms)
-    {
-    }
+    public function __construct(protected float $kilograms) {}
 
     public function toPounds(): float
     {
         return $this->kilograms * 2.2046;
     }
-
 }
-
--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -1,10 +1,9 @@
 <?php
 
-use \Bappy3a\WeightConversion\WeightConversionClass;
+use Bappy3a\WeightConversion\WeightConversionClass;
 
 it('can convert kilograms to pounds', function () {
-    $pounds =  WeightConversionClass::kilograms(100)->toPounds();
-
+    $pounds = WeightConversionClass::kilograms(100)->toPounds();
 
     expect($pounds)->toEqual(220.46);
 });
